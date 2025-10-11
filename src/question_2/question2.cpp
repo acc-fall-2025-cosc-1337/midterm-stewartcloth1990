c1b5a8f7786e94df94ee5f0f43e62b8eab57fd0b
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-11 21:27:36 +0000

    created func prototypes for q2

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,5 +1,10 @@
 #include "question2.h"
 
+bool is_palindrome(const std::string& str)
+{
+    
+}
+
 bool test_config()
 {
     return true;
--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,5 @@
+#include <string>
+
 bool test_config();
+
+bool is_palindrome(const std::string& str);
