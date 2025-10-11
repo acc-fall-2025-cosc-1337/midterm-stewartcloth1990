7221cc8ea03fa35e987490b12512f3e28f307038
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-11 19:49:15 +0000

    minor updates for formatting

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -5,6 +5,7 @@
 using std::cout;
 using std::cin;
 using std::string;
+using std::endl;
 
 int main()
 {
@@ -13,29 +14,29 @@ int main()
     int choice;
     char again = 'y';
 
-    cout << "Fibonacci Sequence" << "\n" << "1 - Get a Fibonacci Number" << "\n" << "2 - Exit the program." << "\n";
+    cout << "Fibonacci Sequence" << "\n" << "1 - Get a Fibonacci Number" << "\n" << "2 - Exit the program." << endl;
     cin >> choice;
 
     if (choice == 1)
     {
         do
         {
-            cout << "Please enter a number between 1 and 15: " << "\n";
+            cout << "Please enter a number between 1 and 15: " << endl;
             cin >> num;
 
             if (num >= 1 && num <= 15)
             {
             get_fib_number = get_fib_sequence(num);
 
-            cout << "The Fibonacci number is " << get_fib_number << "\n";
+            cout << "The Fibonacci number is " << get_fib_number << endl;
 
-            cout << "Would you like to try another number (Y/N): " << "\n";
+            cout << "Would you like to try another number (Y/N): " << endl;
             cin >> again;
             }
 
             else
             {
-                cout << "Invalid number. Please enter a number between 1 and 15." << "\n";
+                cout << "Invalid number. Please enter a number between 1 and 15." << endl;
             }
 
         } while (again == 'y' || again == 'Y');
@@ -43,12 +44,12 @@ int main()
 
     else if (choice == 2)
     {
-		cout << "Exiting program..." << "\n";
+		cout << "Exiting program..." << endl;
 	}
 
     else
 	{
-		cout << "Invalid choice. Please select 1 or 2." << "\n";
+		cout << "Invalid choice. Please select 1 or 2." << endl;
 	}
     
     return 0;
