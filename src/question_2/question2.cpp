9dbcd46e943630ede02cf5b113dd921d473bba06
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-12 16:19:02 +0000

    code for palindrome return true, test cases

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -2,7 +2,20 @@
 
 bool is_palindrome(const std::string& str)
 {
-    
+    int left = 0;
+    int right = str.length() -1;
+
+    while (left < right)
+    {
+        if (str[left] != str[right])
+        {
+            return false;
+        }
+
+        ++left;
+        --right;
+    }
+    return true;
 }
 
 bool test_config()
--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -2,11 +2,20 @@
 #include "catch.hpp"
 #include "question2.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
 TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
+}
+
+TEST_CASE("is_palindrome returns true for palindromes", "[is_palindrome]")
+{
+	REQUIRE(is_palindrome("abcba") == true);
+	REQUIRE(is_palindrome("abc") == false);
+	REQUIRE(is_palindrome("hannah") == true);
+	REQUIRE(is_palindrome("hanna") == false);
 }
