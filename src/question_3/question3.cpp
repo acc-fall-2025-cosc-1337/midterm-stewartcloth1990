aae27f48df9a6cde7de89f619b2f08665befd4a8
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-12 17:14:13 +0000

    code and test cases for q3

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,5 +1,20 @@
 #include "question3.h"
 
+double get_dna_p_distance(const std::string& dna_one, const std::string& dna_two)
+{
+    int diff = 0;
+
+    for (int i = 0; i < dna_one.length(); ++i)
+    {
+        if (dna_one[i] != dna_two[i]) diff++;
+    }
+
+    double ratio = double(diff) / dna_one.length();
+
+    return ratio;
+}
+
+
 bool test_config()
 {
     return true;
--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -10,3 +10,21 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+TEST_CASE("get_dna_p_distance computes correct p-distance", "[get_dna_p_distance]") {
+	SECTION("Test case a") {
+		std::string dna1 = "GAGCCTACTAACGGGAT";
+		std::string dna2 = "CATCGTAATGACGGCCT";
+		double expected = 0.4118;
+		double result = get_dna_p_distance(dna1, dna2);
+		REQUIRE(Approx(result).margin(0.0001) == expected);
+	}
+
+	SECTION("Test case b") {
+		std::string dna1 = "GAGCCTACTAACGGGAT";
+		std::string dna2 = "GATCGTAATGACGGCCT";
+		double expected = 0.3529;
+		double result = get_dna_p_distance(dna1, dna2);
+		REQUIRE(Approx(result).margin(0.0001) == expected);
+	}
+}
