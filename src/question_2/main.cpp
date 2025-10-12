50454c8c1b1f797976147e2c637e7d04b33139c5
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-12 16:45:07 +0000

    main code block q2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,38 @@
+#include "question2.h"
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
+    std::string user_string;
+    char again = 'y';
+
+    do
+    {
+
+        cout << "Please enter a string: " << endl;
+        cin >> user_string;
+
+        bool is_palindrome_result = is_palindrome(user_string);
+
+        if (is_palindrome_result == true)
+        {
+            cout << "True" << endl;
+        }
+
+        else
+        {
+            cout << "False" << endl;
+        }
+
+        cout << "Enter another string? (Y/N): " << endl;
+        cin >> again;
+
+    } while(again == 'y' || again == 'Y');
 
     return 0;
 }
