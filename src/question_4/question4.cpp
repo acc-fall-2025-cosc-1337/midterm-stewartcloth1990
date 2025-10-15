62b9c4f8b823e0735a01b2673248fa5f964149a8
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-15 00:18:14 +0000

    func for dna into rna transcription - q4

--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,3 +1,23 @@
+#include "question4.h"
+#include <string>
+
+using namespace std;
+
+string transcribe_dna_into_rna(const string& dna)
+{
+    string rna = dna;
+
+    for (char& c : rna)
+    {
+        if (c == 'T')
+        {
+            c = 'U';
+        }
+    }
+    return rna;
+}
+
+
 //
 bool test_config()
 {
